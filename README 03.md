f525f17ff23feff7fd1d6eb1ee7a770788836249
Kamran Ali <kgali21@gmail.com>
2019-09-21 12:47:01 -0700

    setting up readme

---
+++ README 03.md
@@ -0,0 +1,38 @@
+# LAB - 02
+
+## Project Classes, Errors, Type Coercion
+
+### Author: Kamran Ali
+
+
+### Links and Resources
+* [submission PR](https://github.com/kgali21/401-lab-01/pull/1)
+* [travis](https://travis-ci.com/401-advanced-javascript-KamranAli/401-lab-01/builds/128578409)
+
+#### Documentation
+* (Server assignments)
+
+### Modules
+#### `pos.js`
+##### Exported Values and Methods
+
+### Setup
+#### `.env` requirements
+
+**or, include an `.env.example`**
+
+#### Running the app
+
+**Describe what npm scripts do**
+  
+#### Tests
+Unit Tests: npm test
+Lint Test: npm run lint
+Jest
+
+incomplete tests
+
+need to test xyz ...
+
+#### UML
+
--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const DocumentCollection = require('./lib/document-collection');
 const documents = new DocumentCollection('./documents');
 
 // write some code to exercise your document collection
-const sample = {
-  key: 'value',
-  sick: true
+const fakeObj = {
+  name: 'Freddie',
+  bandMate: true
 };
 
 
-documents.save(sample);
-// documents.save(fakeObj)
-//   .then(obj => {
-//     console.log('save', obj);
-//   });
+documents.save(fakeObj);
+documents.save(fakeObj)
+  .then(obj => {
+    console.log('save', obj);
+  });
 
-// documents.get(fakeObj.id)
-//   .then(obj => {
-//     console.log('get', obj);
-//   });
+documents.get(fakeObj.id)
+  .then(obj => {
+    console.log('get', obj);
+  });
 
-// documents.getAll()
-//   .then(obj => {
-//     console.log('getAll', obj);
-//   });
+documents.getAll()
+  .then(obj => {
+    console.log('getAll', obj);
+  });
